2523fbaf8488b7ece5ffd54e86b4a2a785873564
rudi9999 <67137156+rudi9999@users.noreply.github.com>
2021-12-13 17:58:49 -0300

    @Rufu99

---
+++ online/monitor_htop.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ $(dpkg --get-selections|grep -w 'htop'|head -1) ]]; then
+	title "INSTALADO HTOP..."
+	if apt install htop -y &>/dev/null ; then
+		print_center -verd "Htop instalado"
+		sleep 2
+	else
+		print_center -verm2 "Htop NO instalado"
+		sleep 2
+		return 1
+	fi
+fi
+htop
