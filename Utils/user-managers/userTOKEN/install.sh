aa6599a06c3ace14136b16f2df9e1e34405ab919
rudi9999 <67137156+rudi9999@users.noreply.github.com>
2024-07-17 17:56:47 -0300

    Create install.sh

---
+++ Utils/user-managers/userTOKEN/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+rm -rf $(pwd)/$0
+
+file="/etc/ADMRufu/sbin/userTOKEN"
+
+[[ -f ${file} ]] && rm $file
+
+wget --no-cache -O $file "https://github.com/rudi9999/ADMRufu/raw/main/Utils/user-managers/userTOKEN/userTOKE"
+
+chmod +x $file
+
+rm -rf /usr/bin/userTOKEN
+
+ln -s $file /usr/bin/userTOKEN
+
+echo "==========================="
+echo "   instalacion completa"
+echo "==========================="
+echo "  use el comando: userTOKEN"
+echo "==========================="
